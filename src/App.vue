f0a3fe1c7875705f12a4808a75de84686fe3fc16
Sergey Prizhimov <snaim@example.com>
2025-06-09 16:48:29 +0800

    * Execution of the task

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import itemsWrapper from "./components/items-wrapper.vue"
+import multiSelection from "./components/selection/multi-selection.vue"
+import singleSelection from "./components/selection/single-selection.vue"
+
+</script>
+
+<template>
+  <div class="common">
+    <div class="common__wrapper">
+      <div style="display: flex; height: 100%;">
+        <multi-selection style="margin-right: 10px;"></multi-selection>
+        <single-selection></single-selection>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.common {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.common__wrapper {
+  padding: 1rem;
+  border: solid 5px black;
+  width: 60vw;
+  height: 80vh;
+}
+</style>
---
+++ src/components/items-wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="items__wrapper">
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<style>
+.items__wrapper {
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+}
+
+.items__wrapper .item {
+  margin: 2px;
+}
+</style>
